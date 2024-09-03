9133d4d5aa7b0fcd2ee46091eb3776f201574b5d
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2024-09-03 13:36:50 +0100

    Update tgetnum.c

--- tgetnum.c
+++ tgetnum.c
@@ -10,21 +10,26 @@
 	
 	name
 
-		"li" - height
-		"co" - width 
-		"it" - intial spacing between hardware tab stop columns
-		"kn" - number of numbered function keys
-		"lm" - lines of display memory
-		"pb" - lowest baud rate which padding is actually needed
-		"sg" - width of magic standout cookie
-		"ug" - width of underlining  magic standout cookie
-		"ws" - width of status line 
-
-		"colors" - number of colors
+		"co" (number of columns in a line)
+		"it" (tabs initially every # spaces)
+		"li" (number of lines on screen or page)
+		"lm" (lines of memory if > line. 0 means varies)
+		"sg" (number of blank characters left by smso or rmso)
+		"pb" (lowest baud rate where padding needed)
+		"vt" (virtual terminal number (CB/unix))
+		"ws" (number of columns in status line)
+		"Nl" (number of labels on screen)
+		"lh" (rows in each label)
+		"lw" (columns in each label)
+		"ma" (maximum combined attributes terminal can handle)
+		"MW" (maximum number of definable windows)
+		"Co" (maximum number of colors on screen)
+		"pa" (maximum number of color-pairs on the screen)
+		"NC" (video attributes that cannot be used with colors)
 
 	return value or -1 on not mention in terminal description 
 	
-	NOTE:	cc tgetnum.c -ltermcap
+	NOTE:	cc tgetent.c -ltermcap
 			infocmp
 			./a.out xterm
 
@@ -32,6 +37,8 @@
 */
 
 #define BUFFER_SIZE 1024
+
+void print_tgetnums(void);
 
 int main(int ac, char **av)
 {
@@ -47,12 +54,7 @@ int main(int ac, char **av)
 	if (result == 1)
 	{
 		printf("Terminal %s: found\n", av[1]);
-		int height;
-		int	width;
-		height = tgetnum("li");
-		width = tgetnum("co");
-		printf("%s Height: %d\n", av[1], height);
-		printf("%s Width: %d\n", av[1], width);
+		print_tgetnums();
 	}
 	else if (result == 0)
 	{
@@ -65,4 +67,24 @@ int main(int ac, char **av)
 		exit(EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
+}
+
+void print_tgetnums(void)
+{
+    printf("Columns: %d\n", tgetnum("co"));
+    printf("Init Tabs: %d\n", tgetnum("it"));
+    printf("Lines: %d\n", tgetnum("li"));
+    printf("Lines of Memory: %d\n", tgetnum("lm"));
+    printf("Magic Cookie Glitch: %d\n", tgetnum("sg"));
+    printf("Padding Baud Rate: %d\n", tgetnum("pb"));
+    printf("Virtual Terminal: %d\n", tgetnum("vt"));
+    printf("Width Status Line: %d\n", tgetnum("ws"));
+    printf("Num Labels: %d\n", tgetnum("Nl"));
+    printf("Label Height: %d\n", tgetnum("lh"));
+    printf("Label Width: %d\n", tgetnum("lw"));
+    printf("Max Attributes: %d\n", tgetnum("ma"));
+    printf("Maximum Windows: %d\n", tgetnum("MW"));
+    printf("Max Colors: %d\n", tgetnum("Co"));
+    printf("Max Pairs: %d\n", tgetnum("pa"));
+    printf("No Color Video: %d\n", tgetnum("NC"));
 }
