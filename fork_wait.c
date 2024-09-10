ded9ddcce085ae800fa33ca848f7dbcfd9d4ba21
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2024-09-10 16:01:20 +0100

    Update fork_wait.c

--- fork_wait.c
+++ fork_wait.c
@@ -1,15 +1,46 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+/*
+	FORK - create a new process
 
-// pid_t	fork(void);
-// pid_t 	wait(int *stat_loc);
+		pid_t	fork(void);
+
+	on success both Parent and Child processes return a pid_t
+	
+	Child returns 0 on success
+	Parent returns a positve, non-zero pid_t on success
+	
+	on failure the parent return -1 (sets errno)
+		EAGAIN - too many processes running
+		ENOMEN - not enough space 
+
+	Attributes
+		- child has its own pid
+		- the child knows the parent's pid, this is referred to as the ppid
+		- child has a copy of parent's open fildes
+		- the elapsed CPU time for child is set to zero
+		- no locks are inherited
+		- no alarms are set
+		- pending signal are cleared
+		- sigaction mask is inherited
+
+	WAIT - 
+
+		pid_t 	wait(int *stat_loc);
+
+*/
 
 int	main(int ac, char **av)
 {
 	pid_t	pid;
 
-	pid = fork();
+	if ((pid = fork()) == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 	if (pid)
 		printf("Parent: %d\n", getpid());
 	if (pid == 0)
