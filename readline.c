942fbab0ff3670ae3a08ecd752bcac9f67f0770b
Skyy Moore <mooreapps2022@gmail.com>
2024-11-12 10:01:30 -0800

    Adjusted Readline

--- readline.c
+++ readline.c
@@ -2,9 +2,20 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-// char *readline(const char *prompt);
-// cc readline.c -lreadline
+/*
+	READLINE - get a line from a user with editing
+	
+	char *readline(const char *prompt);
 
+		Gets a line from the terminal using PROMPT as the prompt
+
+		The return is allocated, so you must free it after done using
+
+		If PROMPT is NULL or an empty string, no prompt is seen by the user
+
+	Necessary Compile Option:
+		 cc readline.c -lreadline
+*/
 int	main(int ac, char **av)
 {
 	char *input;
