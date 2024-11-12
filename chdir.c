ef6bf5cf625e4918489940ecf088f459474c4212
Skyy Moore <mooreapps2022@gmail.com>
2024-11-12 11:26:15 -0800

    Adjusted chdir.

--- chdir.c
+++ chdir.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <readline/readline.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
-	CHANGE DIRECTORY
+	CHANGE DIRECTORY - change working directory
 
 		int	chdir(const char *path);
-		return 0 on sucess and -1 on failure (sets errno)
+
+			Changes the CWD of the calling process to directory PATH
+
+			return 0 on success and -1 on failure (sets errno)
 */
 
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	char	*input;
+
+	while (true)
 	{
-		if (chdir(av[1]) == 0) // make sure the directory exists
-		{
-			while (1)
-			{
-				printf("I'm in %s\n", av[1]);
-			}
-			exit(EXIT_SUCCESS); // you will exit back into the directory you started
-		}
+		input = readline("$> cd ");
+		if (chdir(input))
+			printf("No such directory\n");
 		else
-		{
-			perror("Failed to change directory");
-			exit(EXIT_FAILURE);
-		}
+			printf("You are now in directory %s\n", input);
+		free(input);
 	}
+	return (0);
 }
