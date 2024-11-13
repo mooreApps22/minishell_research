f18c04ff25b4ee2b64b4ca8da5d12b7df951322f
Skyy Moore <mooreapps2022@gmail.com>
2024-11-13 10:35:16 -0800

    dup2 adjusted

--- dup2.c
+++ dup2.c
@@ -1,29 +1,42 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
 	DUP2 - duplicate a file descriptor
 
-	int dup2(int oldfd, int newfd);
+		int dup2(int oldfd, int newfd);
 
-	returns newfd on success and -1 on failure (sets an errno)
+		NEWFD now refers to OLDFD
 
+		returns newfd on success and -1 on failure (sets an errno)
+
+	Usage:
+	./a.out <filename> <message to write to filename>
 */
+
+void	create_new_file(char *filename, char *msg)
+{
+	int oldfd;	
+	int newfd;	
+
+	oldfd = open(filename, O_RDWR | O_CREAT, 0664); // no need to make sure the file is in the directory
+	newfd = dup2(oldfd, newfd);
+	if (newfd == -1)
+		printf("Didn't open dup fd");
+	else
+	{
+		write(newfd, msg, strlen(msg));
+		write(newfd, "\n", 1);
+	}
+	close(newfd);
+	close(oldfd);
+}
 
 int	main(int ac, char **av)
 {
-	int fd1;	
-	int fd2;	
-
-	fd1 = open("test.txt", O_RDWR | O_CREAT, 0664); // no need to make sure the file is in the directory
-	fd2 = dup2(fd1, fd2);
-	if (!fd2)
-		printf("Didn't open dup fd");
-	else
-		printf("The dup fd was opened");
-	close(fd1);
-	close(fd2);
+	create_new_file(av[1], av[2]);
 	printf("\n");
 	return (0);
 }
