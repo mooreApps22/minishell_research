3aae3670431bd1eeb970945205733597c4bf89b7
Skyy Moore <mooreapps2022@gmail.com>
2024-11-14 08:41:32 -0800

    Adjusted open_close

--- open_close.c
+++ open_close.c
@@ -2,19 +2,26 @@
 #include <unistd.h>
 #include <string.h>
 
+
 // int open(const char *pathname, int flags, mode_t mode);
 
 // ./a.out song.txt "all the small things"
 // cat song.txt
 
-int main(int ac, char **av)
+void	open_file(char *name, char *content)
 {
 	int fd;
 
-	fd = open(av[1], O_CREAT | O_RDWR, 0660);
+	fd = open(name, O_CREAT | O_RDWR, 0660);
 	if (fd == -1)
 		write(1, "error\n", 6);
-	write(fd, av[2], strlen(av[2]));
+	write(fd, content, strlen(content));
 	close(fd);
+}
+
+int main(int ac, char **av)
+{
+	open_file(av[1], av[2]);
+	write(1, "\n", 1);
 	return (0);
 }
