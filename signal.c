5990efead5013e6a53a2e107bc5106cb47a81d53
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2024-12-22 13:06:42 +0000

    Update signal.c

--- signal.c
+++ signal.c
@@ -1,41 +1,122 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include "libft/include/libft.h"
+
 
 /*
-	SIGNAL - sets the disposition of the signal signo
+	SIGNAL	- sets the function of a signal
 
-	sighandler_t	signal(int signo, sighandler_t handler);
+		sighandler_t	signal(int sig, sighandler_t handler);
 
-	signal() sets signo to handler or the address of a function
-	- if delivered
-		- disposition is set to SIG_IGN (it's ignored)
-		- set to SIG_DFL (default)
-		- or set to a funciton, then SIG_DFL, or blocked
-		
-	BASICALLY, if signal is caught the handler is called
-	SIGKILL or SIGSTOP cannot be caught or ignored
+		ARGS
 
-	Return 
-		
-		returns previous value of the signal handler or on failute SIG_ERR (errno set)
+			sig - sig is the signal whose function you want to set	
+
+			handler - this is a `void	handler(int sig)` function which with run when sig is used
+
+				the kill() function is call in the prograom or the user inputs the signal
+
+			you have three options for handler
+				
+				1. SIG_IGN - this tells the process to ignore sig
+				2. SIG_DFL - this tells the process to treat sig normally
+				3. handler - this tells the process to redefine sig with the code in the handler
+
+		RETURN
+
+			returns the previous value of the handler or SIG_ERR on error(errno set)
+
+		IMPORTANT NOTES
+
+			if you call signal() and fork() the handler will affect both the parent and child
+			it is best to use signal() inside the child to ignore all signals
+
 */
 
-void	handler(int signo)
+volatile sig_atomic_t	g_signal;
+
+
+void	handle_sigint(int signal)
 {
-	printf(" Why won't you shut up!\n");
+	g_signal = signal;
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	printf("\n");
+	rl_redisplay();
 }
 
-int	main(int ac, char **av)
+void	handle_sigquit(int signal)
 {
-	while (1)
+	g_signal = signal;
+	printf("Ctrl-\\\n");
+	exit(0);
+}
+
+void	config_parent_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	config_child_signals(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	process_input(char **cmdv)
+{
+	char **tmp;
+
+	tmp = cmdv;
+	while (*tmp)
 	{
-		if (signal (SIGINT, handler) == SIG_IGN)
-			signal (SIGINT, SIG_IGN);
-		printf("Sup.\n");
-		usleep(500000);
+		printf("\t%s ...\n", *tmp);
+		tmp++;
 	}
-	return(1);
+	printf("\tYeah... I'm not doing that.\n");
 }
 
+int		main(int ac, char **av)
+{
+	char	*input;
+	char	**cmdv;
+	pid_t	pid;
+
+	config_parent_signals();
+	while (true)
+	{
+		printf("Parent (Before Fork): %d\n", getpid());
+		input = readline("minishell$ ");
+		if (input == NULL || (ft_strncmp(input, "exit", 4) == 0))
+		{
+			free(input);
+			exit(0);
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			config_child_signals();
+			cmdv = ft_split(input, ' ');
+			process_input(cmdv);
+			ft_free_str_arr(cmdv);
+			free(input);
+			printf("Parent PID in Child: %d\n", getppid());
+			printf("Child PID: %d\n", getpid());
+			exit(0);
+		}
+		else
+		{
+			waitpid(pid, NULL, 0);
+			printf("Parent PID (After Fork): %d\n", getpid());
+		}
+	}
+	if (input)
+		free(input);
+	return (0);
+}
